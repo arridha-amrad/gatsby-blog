1f426bbef2bdd07ec949960dd4db8ecfc4c5b5e9
arridhaamrad <arridhaamrad@gmail.com>
2020-05-04 21:21:39 +0700

    Create pages programmatically with gatsby

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -4,7 +4,7 @@
  * See: https://www.gatsbyjs.org/docs/ssr-apis/
  */
 
-// You can delete this file if you're not using it
+/// You can delete this file if you're not using it
 
 import React from "react"
 import { ThemeProvider } from "styled-components"
